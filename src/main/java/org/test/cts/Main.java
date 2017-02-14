592eab920a4a918102eb3f377b8f957b9942e007
Amit Kumar Chaudhary <amitchaudhary.it@gmail.com>
2017-02-14 23:41:19 +0530

    Added all functionlty

--- src/main/java/org/test/cts/Main.java
+++ src/main/java/org/test/cts/Main.java
@@ -1,12 +1,25 @@
 package org.test.cts;
 
 import java.net.URI;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.atlassian.jira.rest.client.api.JiraRestClient;
 import com.atlassian.jira.rest.client.api.JiraRestClientFactory;
 import com.atlassian.jira.rest.client.api.domain.BasicProject;
+import com.atlassian.jira.rest.client.api.domain.Comment;
 import com.atlassian.jira.rest.client.api.domain.Issue;
+import com.atlassian.jira.rest.client.api.domain.IssueFieldId;
 import com.atlassian.jira.rest.client.api.domain.SearchResult;
+import com.atlassian.jira.rest.client.api.domain.Transition;
 import com.atlassian.jira.rest.client.api.domain.User;
+import com.atlassian.jira.rest.client.api.domain.input.ComplexIssueInputFieldValue;
+import com.atlassian.jira.rest.client.api.domain.input.FieldInput;
+import com.atlassian.jira.rest.client.api.domain.input.IssueInput;
+import com.atlassian.jira.rest.client.api.domain.input.LinkIssuesInput;
+import com.atlassian.jira.rest.client.api.domain.input.TransitionInput;
 import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
 import com.atlassian.util.concurrent.Promise;
  
@@ -32,11 +45,22 @@ public class Main
     	final  Promise<SearchResult>  searchresule=restClient.getSearchClient().searchJql(jql);
     	final  SearchResult reult=  searchresule.claim();
     	 System.out.println(reult);
-    	final   Promise<Issue> promiseIssue = restClient.getIssueClient().getIssue("ATS-6");
-    	
+    	final   Promise<Issue> promiseIssue = restClient.getIssueClient().getIssue("ATS-1");
     	final  Issue issue = promiseIssue.claim();
+    	
+ 
+    	final Comment comment = Comment.valueOf("BuildID: 20121022-14000-2222222\nSystem: \nVersion: 2012 Q3\nTell me more");
+    	restClient.getIssueClient().addComment(issue.getCommentsUri(), comment).claim();
     	System.out.println(issue);
-       
+    	
+    	Map<String, FieldInput> fields= new HashMap<String, FieldInput>();
+    	fields.put("description", new FieldInput("description", "Testing man AMit Chaudhary and Amit Alexis"));
+    	IssueInput issuein= new IssueInput(fields);
+    	restClient.getIssueClient().updateIssue("ATS-1", issuein).claim();
+    	
+    	
+    	// For Linking Issues 
+    	restClient.getIssueClient().linkIssue(new LinkIssuesInput("ATS-1","ATS-3","Duplicate",Comment.valueOf("issue Added comment"))).claim();
         
     /*    for (BasicProject project : restClient.getProjectClient().getAllProjects().claim()) {
             System.out.println(project.getKey() + ": " + project.getName());
